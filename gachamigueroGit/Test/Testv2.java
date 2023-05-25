773ef6d43f96b620b87ef6ae6520cbad3eb7bc5a
Sergio GIT <serinical@alu.edu.gva.es>
2023-05-25 12:23:38 +0200

    test 3

--- gachamigueroGit/Test/Testv2.java
+++ gachamigueroGit/Test/Testv2.java
@@ -8,8 +8,14 @@ public class Testv2 {
         String result=v2.cocinar(1);
         Assertions.assertEquals("1",result);
     }
+    @Test
     public void test2(){
         String result=v2.cocinar(2);
         Assertions.assertEquals("2",result);
+    }
+    @Test
+    public void test3(){
+        String result=v2.cocinar(3);
+        Assertions.assertEquals("Gacha",result);
     }
 }
