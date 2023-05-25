c884efb0f921cc74188894cc319af741c16d02df
Sergio GIT <serinical@alu.edu.gva.es>
2023-05-25 12:38:48 +0200

    Pruebas v3

---
+++ gachamigueroGit/Test/Testv3.java
@@ -0,0 +1,85 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Testv3{
+    @Test
+    public void test1gach(){
+        String result=v3.catar(1);
+        Assertions.assertEquals("1",result);
+    }
+    @Test
+    public void test2gach(){
+        String result=v3.catar(2);
+        Assertions.assertEquals("2",result);
+    }
+    @Test
+    public void test3gach(){
+        String result=v3.catar(3);
+        Assertions.assertEquals("Gacha",result);
+    }
+    @Test
+    public void test4gach(){
+        String result=v3.catar(4);
+        Assertions.assertEquals("4",result);
+    }
+    @Test
+    public void test5gach(){
+        String result=v3.catar(5);
+        Assertions.assertEquals("Miga",result);
+    }
+    @Test
+    public void test6gach(){
+        String result=v3.catar(6);
+        Assertions.assertEquals("Gacha",result);
+    }
+    @Test
+    public void test7gach(){
+        String result=v3.catar(7);
+        Assertions.assertEquals("7",result);
+    }
+    @Test
+    public void test8gach(){
+        String result=v3.catar(8);
+        Assertions.assertEquals("8",result);
+    }
+    @Test
+    public void test9gach(){
+        String result=v3.catar(9);
+        Assertions.assertEquals("Gacha",result);
+    }
+    @Test
+    public void test10gach(){
+        String result=v3.catar(10);
+        Assertions.assertEquals("Miga",result);
+    }
+    @Test
+    public void test11gach(){
+        String result=v3.catar(11);
+        Assertions.assertEquals("11",result);
+    }
+    @Test
+    public void test12gach(){
+        String result=v3.catar(12);
+        Assertions.assertEquals("Gacha",result);
+    }
+    @Test
+    public void test13gach(){
+        String result=v3.catar(13);
+        Assertions.assertEquals("13",result);
+    }
+    @Test
+    public void test14gach(){
+        String result=v3.catar(14);
+        Assertions.assertEquals("14",result);
+    }
+    @Test
+    public void test15gach(){
+        String result=v3.catar(15);
+        Assertions.assertEquals("Gachamiga",result);
+    }
+    @Test
+    public void test30gach(){
+        String result=v3.catar(30);
+        Assertions.assertEquals("Gachamiga",result);
+    }
+}
